b19172c90e3bf9b136641075a1dd7a366650319e
hassain-saeed28 <105624601+hassain-saeed28@users.noreply.github.com>
2022-12-05 21:19:17 +0300

    working on the main meun and section calsses

--- ICS-108-Project-term-221/src/MainMenu.java
+++ ICS-108-Project-term-221/src/MainMenu.java
@@ -1,18 +1,17 @@
-import java.util.ArrayList;
+import java.io.FileNotFoundException;
 
 import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
-import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.ListView;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.Background;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.ColumnConstraints;
-import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
-import javafx.scene.layout.Pane;
-import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Paint;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
@@ -23,7 +22,7 @@ public class MainMenu extends Application {
      * @see javafx.application.Application#start(javafx.stage.Stage)
      */
     @Override
-    public void start(Stage primaryStage) {
+    public void start(Stage primaryStage) throws FileNotFoundException {
 
         // declaring the main page pane wich is a border pane 
         BorderPane borderPane = new BorderPane();
@@ -35,6 +34,8 @@ public class MainMenu extends Application {
         hBoxTop.setPadding(new Insets(15,15,15,15));
         // seting spacing between the combonents of the the HBox
         hBoxTop.setSpacing(100);
+        hBoxTop.setBackground(Background.fill(Paint.valueOf("ffff")));
+
 
         Text titel = new Text("Add Sections to Basket");
         titel.setFont(Font.font(30));
@@ -46,22 +47,52 @@ public class MainMenu extends Application {
         hBoxTop.getChildren().addAll(titel,topRightButton);
 
 
-        ListView<Object> listViewSectionsOfCourses = new ListView<>();
+        // ArrayList<Section> sectinosMenu = Section.sections;
+        // System.out.println(sectinosMenu.get(0));
+        // listViewSectionsOfCourses.getItems().addAll(addButton,removeButton);
+        TableView<Section> tableView = new TableView<>();
 
-        Button addButton = new Button("add");
-        Button removeButton = new Button("Remove");
+        TableColumn<Section,String> courseColumn = new TableColumn<>("Course");
+        // courseColumn.setMinWidth(200);
+        courseColumn.setCellValueFactory(new PropertyValueFactory<>("course"));
 
-        // listViewSectionsOfCourses.getItems().add(0, removeButton);
-        // System.out.println(Section.sections);
+        TableColumn<Section,String> secColumn = new TableColumn<>("Section");
+        // secColumn.setMinWidth(200);
+        secColumn.setCellValueFactory(new PropertyValueFactory<>("sec"));
 
-        ArrayList<Section> sectinosMenu = Section.sections;
-        System.out.println(sectinosMenu.get(0));
-        listViewSectionsOfCourses.getItems().addAll(addButton,removeButton);
+        TableColumn<Section,String> daysColumn = new TableColumn<>("Days");
+        // daysColumn.setMinWidth(200);
+        daysColumn.setCellValueFactory(new PropertyValueFactory<>("days"));
+
+        TableColumn<Section,String> timeColumn = new TableColumn<>("Time");
+        // timeColumn.setMinWidth(200);
+        timeColumn.setCellValueFactory(new PropertyValueFactory<>("time"));
+
+        TableColumn<Section,String> locationColumn = new TableColumn<>("Location");
+        // locationColumn.setMinWidth(200);
+        locationColumn.setCellValueFactory(new PropertyValueFactory<>("location"));
+
+        TableColumn<Section,Button> addBottonColumn = new TableColumn<>("Adding botton");
+        // addBottonColumn.setMinWidth(200);
+        addBottonColumn.setCellValueFactory(new PropertyValueFactory<>("addButton"));
+
+        TableColumn<Section,Button> removeButtonColumn = new TableColumn<>("RemovebButton");
+        // removeButtonColumn.setMinWidth(200);
+        removeButtonColumn.setCellValueFactory(new PropertyValueFactory<>("removeButton"));
+
+
+        tableView.setItems(Section.getSectionsObservableList());
+        tableView.getColumns().addAll(courseColumn,secColumn,daysColumn,timeColumn,locationColumn,addBottonColumn,removeButtonColumn); 
+        tableView.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY);
+        tableView.setPadding(new Insets(0, 10, 0, 10));
+        tableView.setBackground(Background.fill(Paint.valueOf("ffff")));
+
 
 
 
         HBox hBoxBottom = new HBox();
         hBoxBottom.setAlignment(Pos.BOTTOM_RIGHT);
+        hBoxBottom.setBackground(Background.fill(Paint.valueOf("ffff")));
         hBoxBottom.setPadding(new Insets(15,15,15,15));
 
         Button nextButton =  new Button("next");
@@ -73,7 +104,7 @@ public class MainMenu extends Application {
 
         borderPane.setTop(hBoxTop);
         borderPane.setBottom(hBoxBottom);
-        borderPane.setCenter(listViewSectionsOfCourses);
+        borderPane.setCenter(tableView);
 
         Scene scene = new Scene(borderPane, 900, 700);   
         primaryStage.setTitle("ShowImage"); // Set the stage title
--- ICS-108-Project-term-221/src/Section.java
+++ ICS-108-Project-term-221/src/Section.java
@@ -1,52 +1,88 @@
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
 import java.util.Scanner;
 
-
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.scene.control.Button;
 
 public class Section {
 
     // the list of sectins in the course offering
-    public static ArrayList<Section> sections = new ArrayList<>();
+    public static ObservableList<Section> sections = FXCollections.observableArrayList() ;
 
-    private String course_sec;
+    private String course;
+    private String sec;
+    private String days;
+    private String time;
     private String location;
-    private String days_time;
+    private Button addButton;
+    private Button removeButton;
+
 
     public Section() {
 
     }
 
     public Section(
-            String course_sec,
-            String location,
-            String days_time) {
-        this.course_sec = course_sec;
+            String course,
+            String sec,
+            String days,
+            String time,
+            String location) {
+        this.course = course;
+        this.sec = sec;
+        this.days = days;
+        this.time = time;
         this.location = location;
-        this.days_time = days_time;
+        this.addButton = new Button("add");
+        this.removeButton = new Button("Remove");
+        removeButton.setOnAction(e->{
+            System.out.println("it works too");
+        });
+        addButton.setOnAction(e->{
+            System.out.println("it works");
+        });
     }
 
-    public String getTime_days() {
-        return days_time;
+    public String getCourse() {
+        return course;
     }
 
-    public String getCourse_sec() {
-        return course_sec;
+    public String getSec() {
+        return sec;
+    }
+
+    public String getDays() {
+        return days;
+    }
+
+    public String getTime() {
+        return time;
     }
 
     public String getLocation() {
         return location;
     }
 
-    @Override
-    public String toString() {
-        return "\nthe course and the section are: " + this.course_sec +
-                "\nthe location is: " + this.location +
-                "\nthe time and days are: " + this.days_time;
+    public Button getAddButton() {
+        return addButton;
+    }
+    public Button getRemoveButton() {
+        return removeButton;
     }
 
-    public static void main(String[] args) throws FileNotFoundException {
+    @Override
+    public String toString() {
+        return "\ncourse: " + this.course +
+                "section: " + this.sec +
+                "days: " + this.days +
+                "time: " + this.time +
+                "\nthe location is: " + this.location;
+    }
+
+    public static ObservableList<Section> getSectionsObservableList() throws FileNotFoundException {
+
 
         // opening the course offiering file and adding each section in the list of
         // sections
@@ -56,9 +92,6 @@ public class Section {
         // to skep the first row because is has the labels
         input.nextLine();
 
-        // just for verifying the it works (the method below)
-        int index = 0;
-
         // here i will read each line and then split it by the "," dilamitr and then
         // store the components in an array then make a Section opject and then store it
         // in the arrayList sectinos
@@ -66,18 +99,18 @@ public class Section {
 
             String line = input.nextLine();
             String[] lineComponents = line.split(",");
-
-            String Course = new String(lineComponents[0]);
+            String[] course_sec = lineComponents[0].split("-");
+            String course = new String(course_sec[0]);
+            String sec = new String(course_sec[1]);
+            String days = new String(lineComponents[2]);
+            String time = new String(lineComponents[3]);
             String location = new String(lineComponents[1]);
-            String day_time = new String(lineComponents[3] + " " + lineComponents[2]);
 
-            sections.add(new Section(Course, location, day_time));
-
-            System.out.println(sections.get(index));
-            index++;
+            sections.add(new Section(course, sec, days, time, location));
         }
-
         input.close();
+        return sections;
+
     }
 
 }
